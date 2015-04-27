1876b95584a30cec340f2ac915ab5d1117543842
Evelyn Lester <sashcrane@gmail.com>
2015-04-27 18:22:51 +0000

    Updated PHP

--- data/converse-directory.php
+++ data/converse-directory.php
@@ -1,3 +1,16 @@
 <?php
+if ($student-dir)
+    if ($student) {
+        echo firstname, lastname, preferred, email, homestate;
+     }
+    elseif ($faculty){
+        echo firstname, lastname, middlename, preferred, email, homestate, classification, degree, major1, major2, major3, org1, org2, org3;
+    }
+    else() {
+        echo preferred, lastname, email;
+    }
+else ($fac-staff-dir) {
+    echo lastname, firstname, jobtitle, dept, building, room, email, phone;
+   } 
 
 ?>
