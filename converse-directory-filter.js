71f100f7386408bd7b84bc714e8a3f0ee9ae96e2
phbrownacmorg <phbrown@acm.org>
2015-05-07 18:29:15 +0000

    Stab #2 at filtering

--- converse-directory-filter.js
+++ converse-directory-filter.js
@@ -8,16 +8,13 @@ var colTitles = {'lastname': 'Last name', 'firstname': 'First name', 'preferred'
 
 //Begin filter (Tutorial from http://code.tutsplus.com/tutorials/using-jquery-to-manipulate-and-filter-data--net-5351)
 var filterFn = function (query, column) {
-    query = $.trim(query); //trim white space
-    //query = query.replace(/ /gi, '|'); //add OR for regex query
-    query = new RegExp(query, 'i'); // Case-insensitive
-    //alert(query);
-
-    $('tbody tr:has("td")').each(function() {
+    //console.log("filterFn called");
+    $('tr.person').each(function() {
         var selector = $(this);
         if (column !== undefined) {
             selector = $(this).children('.' + column);
         }
+        //console.log("in loop");
         if (query.test(selector.text())) {
             $(this).show().addClass('visible');
         }
@@ -27,51 +24,55 @@ var filterFn = function (query, column) {
     });
 };
     
+var loadTable = function(data, filterFunc) {
+    //alert('Loading table');
+    // Get rid of all the table rows
+    //$('#results tr').remove();
+    
+    // Insert the rows from the given data object
+    //   First, the header row
+    $('#results table').append('<tr class="header"></tr>');
+    var cols = ['lastname', 'firstname', 'preferred', 'middlename', 'classification', 'email', 'phone', 'building', 
+                'room', 'jobtitle', 'dept', 'homeaddress', 'homecity', 'homestate', 'homezip', 'homephone'];
+    //alert(cols);
+    //alert(colTitles);
+    for (var i = 0; i < cols.length; i++) {
+        //alert(cols[i] + ' ' + colTitles[cols[i]]);
+        // There is only one tr at this point
+        $('#results tr').append('<th></th>');
+        $('#results tr > th:last').html(colTitles[cols[i]]);
+    }
+    //alert('Table done');
+    
+    // Fill the rows
+    for (var j = 0; j < data.length; j++) {
+        $('#results table').append('<tr class="person"></tr>');
+        for (var i = 0; i < cols.length; i++) {
+            $('#results tr:last').append('<td class="' + cols[i] + '"></td>');
+            //$('#results tr:last > td:last').addClass([cols[i]]);
+            // if ((j === 0) && (i === 3)) {
+            //     alert('#results tr:last > .' + cols[i]);
+            // }
+            $('#results tr:last > .' + cols[i]).html(data[j][cols[i]]);
+        }
+    }
+    //console.log("Calling filterFunc");
+    filterFunc();
+    $('#results tbody tr').addClass('visible');
+};
+
 $(document).ready(function() {
     // Function for displaying the data in a table
-    var loadTable = function(data, filterFn) {
-        //alert('Loading table');
-        // Get rid of all the table rows
-        //$('#results tr').remove();
-        
-        // Insert the rows from the given data object
-        //   First, the header row
-        $('#results table').append('<tr class="header"></tr>');
-        var cols = ['lastname', 'firstname', 'preferred', 'middlename', 'classification', 'email', 'phone', 'building', 
-                    'room', 'jobtitle', 'dept', 'homeaddress', 'homecity', 'homestate', 'homezip', 'homephone'];
-        //alert(cols);
-        //alert(colTitles);
-        for (var i = 0; i < cols.length; i++) {
-            //alert(cols[i] + ' ' + colTitles[cols[i]]);
-            // There is only one tr at this point
-            $('#results tr').append('<th></th>');
-            $('#results tr > th:last').html(colTitles[cols[i]]);
-        }
-        //alert('Table done');
-        
-        // Fill the rows
-        for (var j = 0; j < data.length; j++) {
-            $('#results table').append('<tr></tr>');
-            for (var i = 0; i < cols.length; i++) {
-                $('#results tr:last').append('<td class="' + cols[i] + '"></td>');
-                //$('#results tr:last > td:last').addClass([cols[i]]);
-                // if ((j === 0) && (i === 3)) {
-                //     alert('#results tr:last > .' + cols[i]);
-                // }
-                $('#results tr:last > .' + cols[i]).html(data[j][cols[i]]);
-            }
-        }
-    };
     
     // Get data
-    var dirdata = $.getJSON('data/data-anyone.json', function(data) {
-        //alert('reading...');
-        loadTable(data, null);
-        //alert('read.');
-    });
+    // var dirdata = $.getJSON('data/data-anyone.json', function(data) {
+    //     //alert('reading...');
+    //     loadTable(data, function() {
+    //         filterFn(/Faculty|Staff/i, 'classification');
+    //     });
+    //     //alert('read.');
+    // });
     
-    
-    $('tbody tr').addClass('visible');
     $('#searchstring').keyup(function(event) {
         if (event.keyCode == 27 || $(this).val() == '') {
          //if esc is pressed we want to clear the value of search box
@@ -79,7 +80,12 @@ $(document).ready(function() {
             $('tbody tr').removeClass('visible').show().addClass('visible');
         }
         else {
-            filterFn($(this).val());
+            var query = $.trim($(this).val()); //trim white space
+            //query = query.replace(/ /gi, '|'); //add OR for regex query
+            query = new RegExp(query, 'i'); // Case-insensitive
+            //alert(query);
+
+            filterFn(query);
         }
     });
     
